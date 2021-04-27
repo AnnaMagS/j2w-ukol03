b36db9902fe96ce5bc458b3eab2128fc4fb6ccd7
18-1855-1 <annamagdalena.stranska@unicorncollege.cz>
2021-04-27 22:25:56 +0200

    uprava controller

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -1,16 +1,38 @@
 package cz.czechitas.java2webapps.ukol3.controller;
 
+import cz.czechitas.java2webapps.ukol3.entity.Vizitka;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Kontroler obsluhující zobrazování vizitek.
  */
 @Controller
-public class VizitkaController {
-  @GetMapping("/")
-  public String seznam() {
-    return "seznam";
+@RequestMapping("/vizitky")
+public class  VizitkaController {
+  private final List<Vizitka> vizitky;
+
+  public VizitkaController() {
+    vizitky = Arrays.asList(
+            new Vizitka("Harry Potter","Bradavice","Příčná 1","11000 Praha 1",
+                    "harry.potter@bradavice.wiz","111 222 333","https://cs.wikipedia.org/wiki/Harry_Potter"),
+            new Vizitka("Frodo Pytlík","Společenstvo prstenu s.r.o","Šedivny 22","51801 Dobré" ,
+                    "","111 333 333",""),
+            new Vizitka("Anakin Skywalker","Rada Jedi","Strahovská 205","11801 Praha 1",
+                    "anakin.skywalker@jedi.uni","","")
+    );
+  }
+
+
+
+
+  /*  @GetMapping("/")
+public String seznam() {
+    return "seznam";*/
   }
 
   @GetMapping("/detail")
@@ -18,3 +40,5 @@ public class VizitkaController {
     return "detail";
   }
 }
+
+
---
+++ src/main/java/cz/czechitas/java2webapps/ukol3/entity/Vizitka.java
@@ -0,0 +1,90 @@
+package cz.czechitas.java2webapps.ukol3.entity;
+
+public class Vizitka {
+    private String name;
+    private String firma;
+    private String ulice;
+    private String PSC;
+    private String celaAdresa;
+    private String email;
+    private String telefon;
+    private String web;
+
+
+    public Vizitka() {
+    }
+
+    public Vizitka(String name, String firma, String ulice, String PSC, String email, String telefon, String web) {
+        this.name = name;
+        this.firma = firma;
+        this.ulice = ulice;
+        this.PSC = PSC;
+        this.email = email;
+        this.telefon = telefon;
+        this.web = web;
+    }
+
+    public String getName() {
+        if (name == null){
+            throw new IllegalArgumentException();
+        }
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirma() {
+        return firma;
+    }
+
+    public void setFirma(String firma) {
+        this.firma = firma;
+    }
+
+    public String getUlice() {
+        return ulice;
+    }
+
+    public void setUlice(String ulice) {
+        this.ulice = ulice;
+    }
+
+    public String getPSC() {
+        return PSC;
+    }
+
+    public void setPSC(String PSC) {
+        this.PSC = PSC;
+    }
+
+    public String getCelaAdresa() {
+        return ulice + "\n" + PSC;
+    }
+
+
+    public String getWeb() {
+        return web;
+    }
+
+    public void setWeb(String web) {
+        this.web = web;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+}
